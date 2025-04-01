108fd2dc88e566e9d952122fa854834658100c75
Andrew Foden <A.Foden2322@student.leedsbeckett.ac.uk>
2025-04-01 06:15:11 +0100

    Created phishing_test_data.txt and phishing_train_data.txt

--- URLDataLoader.py
+++ URLDataLoader.py
@@ -1,8 +1,20 @@
 import pandas as pd
 
 
+def SavePhishingURLs():
+
+    phishing_URLs = df[df['label'] == 0]
+    phishing_URLs= phishing_URLs.sample(50000, random_state=42)
+
+    split_ratio = 0.8
+    split_index = int(split_ratio * len(phishing_URLs))
+
+    phishing_train_data = phishing_URLs[:split_index]
+    phishing_test_data = phishing_URLs[split_index:]
+
+    phishing_train_data.to_csv("phishing_train_data.txt", index=False, header=False)
+    phishing_test_data.to_csv("phishing_test_data.txt", index=False, header=False)
+
+
 df = pd.read_csv("PhiUSIIL_Phishing_URL_Dataset.csv")
-
-first_value = df.iloc[0, 0]
-print("First cell value:", first_value)
-
+SavePhishingURLs()
