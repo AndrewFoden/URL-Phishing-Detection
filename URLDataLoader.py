3aee966dea906661494b8f7d5fb9d6896e151960
Andrew Foden <A.Foden2322@student.leedsbeckett.ac.uk>
2025-04-01 06:20:33 +0100

    Created benign_test_data.txt and benign_train_data.txt

--- URLDataLoader.py
+++ URLDataLoader.py
@@ -15,6 +15,20 @@ def SavePhishingURLs():
     phishing_train_data.to_csv("phishing_train_data.txt", index=False, header=False)
     phishing_test_data.to_csv("phishing_test_data.txt", index=False, header=False)
 
+def SaveBenignURLs():
+
+    benign_URLs = df[df['label'] == 1]
+    benign_URLs= benign_URLs.sample(50000, random_state=42)
+
+    split_ratio = 0.8
+    split_index = int(split_ratio * len(benign_URLs))
+
+    benign_train_data = benign_URLs[:split_index]
+    benign_test_data = benign_URLs[split_index:]
+
+    benign_train_data.to_csv("benign_train_data.txt", index=False, header=False)
+    benign_test_data.to_csv("benign_test_data.txt", index=False, header=False)
 
 df = pd.read_csv("PhiUSIIL_Phishing_URL_Dataset.csv")
-SavePhishingURLs()
+#SavePhishingURLs()
+SaveBenignURLs()
