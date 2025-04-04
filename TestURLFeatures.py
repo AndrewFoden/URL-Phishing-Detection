fceef12f1d9bfff34c0fe1e62c8786e0e4bcc2f8
Andrew Foden <A.Foden2322@student.leedsbeckett.ac.uk>
2025-04-04 11:58:05 +0100

    If length of URL is over 30 label as phishing

--- TestURLFeatures.py
+++ TestURLFeatures.py
@@ -1,17 +1,29 @@
 
 
+URL = []
+predictphishing = 0
+predictbenign = 0
+
 def featureURLLength(URL):
-    phishing = 0
-    benign = 0
-    if (URL > 30):
-        phishing = phishing + 1
+    #print(URL)
+    if len(URL) > 30:
+        label = "phishing"
     else:
-        benign = benign + 1
+        label = "benign"
+    return label
 
 
 
+#benignURLtrain = open('benign_train_data.txt', 'r')
+#phishingURLtrain = open('OnlyURL_phishing_train_data.txt', 'r')
+with open('OnlyURL_phishing_train_data.txt', 'r') as phishingURLtrain:
+    for line in phishingURLtrain:
+        #URL.append(line)
+        label = featureURLLength(line)
+        if label == "phishing":
+            predictphishing += 1
+        else:
+            predictbenign += 1
 
-benignURLtrain = open('benign_train_data.txt', 'r')
-phishingURLtrain = open('phishing_train_data.txt', 'r')
-URL = 10
-featureURLLength(URL)
+print(predictphishing)
+print(predictbenign)
