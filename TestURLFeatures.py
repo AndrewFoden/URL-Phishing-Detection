76ddded120cf22dd5a9062fb2b18f3f472bbbe61
Andrew Foden <A.Foden2322@student.leedsbeckett.ac.uk>
2025-04-08 13:21:17 +0100

    Tested URL Length

--- TestURLFeatures.py
+++ TestURLFeatures.py
@@ -1,29 +1,51 @@
 
 
-URL = []
-predictphishing = 0
-predictbenign = 0
 
-def featureURLLength(URL):
+TP = 0 #TruePhishing = Labelled as phishing, is phishing
+FP = 0 #FalsePhishing = Labelled as phishing, is Benign
+TB = 0 #TrueBenign = Labelled as benign, is benign
+FB = 0 #FalseBenign = Labelled as benign, is phishing
+
+
+def featureURLLength(URL, label):
     #print(URL)
+    decision = ""
     if len(URL) > 30:
-        label = "phishing"
+        if label == "0":
+            decision = "TruePhishing"
+        if label == "1":
+            decision = "FalseBenign"
     else:
-        label = "benign"
-    return label
+        if label == "0":
+            decision = "FalsePhishing"
+        if label == "1":
+            decision = "TrueBenign"
+
+    return decision
+
+#label 1 = benign
+#label 0 = phishing
 
 
 
 #benignURLtrain = open('benign_train_data.txt', 'r')
 #phishingURLtrain = open('OnlyURL_phishing_train_data.txt', 'r')
-with open('OnlyURL_phishing_train_data.txt', 'r') as phishingURLtrain:
-    for line in phishingURLtrain:
-        #URL.append(line)
-        label = featureURLLength(line)
-        if label == "phishing":
-            predictphishing += 1
-        else:
-            predictbenign += 1
+with open('URL_train_data.txt', 'r') as URLtraindata:
+    for line in URLtraindata:
+        url_label = line.strip().split(",")
+        decision = featureURLLength(url_label[0], url_label[1])
 
-print(predictphishing)
-print(predictbenign)
+        if decision == "TruePhishing":
+            TP += 1
+        elif decision == "FalsePhishing":
+            FP += 1
+        elif decision == "TrueBenign":
+            TB += 1
+        elif decision == "FalseBenign":
+            FB += 1
+
+
+print("TruePhishing: " + str(TP))
+print("FalsePhishing: " + str(FP))
+print("TrueBenign: " + str(TB))
+print("FalseBenign: " + str(FB))
