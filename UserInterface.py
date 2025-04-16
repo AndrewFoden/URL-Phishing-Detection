1ff5c4a2748eeb73ea958097c42581431a5bd4bd
Andrew Foden <A.Foden2322@student.leedsbeckett.ac.uk>
2025-04-16 18:41:40 +0100

    Changed layout of window

--- UserInterface.py
+++ UserInterface.py
@@ -9,15 +9,31 @@ class UserInterface:
         root = tk.Tk()
         root.title("URL Phishing Detector")
         root.geometry("700x500")
-        root.configure(bg="lightblue")
+        root.configure(bg="lightgrey")
 
-        label = tk.Label(root, text="Enter a URL:", font=("Arial",20))
-        label.pack(pady=230)
 
-        entry_box = tk.Entry(root, width=40)
-        entry_box.place(x=250,y=300)
+
+        title = tk.Label(root, text="URL Phishing Detector", font=("Arial",30))
+        title.place(x=150,y=40)
+
+        box = tk.Frame(root, bd=3, relief="solid", bg="darkgrey")
+        box.place(x=100, y=170, width=500, height=300)
+
+        label = tk.Label(root, text="Input a URL:", font=("Arial",20))
+        label.place(x=140,y=230)
+
+        self.entry_box = tk.Entry(root, width=40)
+        self.entry_box.place(x=310,y=250)
+
+        entry_button = tk.Button(root, text="Enter", font=("Arial", 14), command=self.getInput())
+        entry_button.place(x=300,y=320)
+
 
         root.mainloop()
+
+    def getInput(self):
+        input = self.entry_box.get()
+        print(input)
 
 
 main = UserInterface()
