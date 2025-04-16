cf887b54bd96e42fc710ba8ed5cbd2b14592aa96
Andrew Foden <A.Foden2322@student.leedsbeckett.ac.uk>
2025-04-16 08:21:09 +0100

    Added to LoadWindow method

--- UserInterface.py
+++ UserInterface.py
@@ -9,6 +9,14 @@ class UserInterface:
         root = tk.Tk()
         root.title("URL Phishing Detector")
         root.geometry("700x500")
+        root.configure(bg="lightblue")
+
+        label = tk.Label(root, text="Enter a URL:", font=("Arial",20))
+        label.pack(pady=230)
+
+        entry_box = tk.Entry(root, width=40)
+        entry_box.place(x=250,y=300)
+
         root.mainloop()
 
 
