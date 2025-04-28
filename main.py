cf794d880858027ef10100ad7ebc0ff8d61a1869
Andrew Foden <A.Foden2322@student.leedsbeckett.ac.uk>
2025-04-28 09:51:09 +0100

    Main class completed

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ from UserInterface import UserInterface
 
 class Main:
 
-    def __init__(self):
+    def __init__(self, extractor):
         #self.Training()
-        print("init")
+        #print("init")
         self.url_input = None
-        self.Application()
+        self.extracted_features = []
+        #self.openDataSet()  #Tests Model
+        self.Application() #Runs Application
+
 
     def Training(self):
         url_labels = self.openDataSet()
@@ -20,12 +23,45 @@ class Main:
 
     def openDataSet(self):
         dataset = []
-        with open('URL_train_data.txt', 'r') as URLtraindata:
+        correctNoPrediction = 0
+        incorrectNoPrediction = 0
+        TP = 0
+        TB = 0
+        FP = 0
+        FB = 0
+        extractor = ApplicationFeatureExtraction()
+        model = DecisionTree()
+        with open('URL_test_data.txt', 'r') as URLtraindata:
             for line in URLtraindata:
                 url_label = line.strip().rsplit(",", 1)
 
-                dataset.append(url_label)
-        return dataset
+                features = extractor.getFeatures(url_label[0])
+               # print(features)
+                decision = model.runUserInput(features)
+                #print(decision)
+                if decision == "phishing" and url_label[1] == "0":
+                    correctNoPrediction += 1
+                    TP += 1
+                elif decision == "benign" and url_label[1] == "1":
+                    correctNoPrediction += 1
+                    TB += 1
+                elif decision == "phishing" and url_label[1] == "1":
+                    incorrectNoPrediction += 1
+                    FP += 1
+                elif decision == "benign" and url_label[1] == "0":
+                    incorrectNoPrediction += 1
+                    FB += 1
+
+        print("Model Test")
+        print("True Phishing: " + str(TP))
+        print("True Benign: " + str(TB))
+        print("False Phishing: " + str(FP))
+        print("False Benign: " + str(FB))
+        print("Correctly predicted: " + str(correctNoPrediction))
+        print("Incorrectly predicted: " + str(incorrectNoPrediction))
+        total = TP + TB + FP + FB
+        accuracy = (correctNoPrediction / total) * 100
+        print("Accuracy: " + str(accuracy) + "%")
 
     def callModel(self, url_labels):
         extractor = FeatureExtraction()
@@ -35,16 +71,25 @@ class Main:
             model.runTraining(url_label[0], url_label[1])
 
     def Application(self):
-        interface = UserInterface(self.urlInput)
-
-        interface.LoadWindow()
+        interface = UserInterface(input_callback = self.urlInput)
+        #print("Application")
+        #interface.LoadWindow()
 
 
     def urlInput(self, urlinput):
-        extractor = ApplicationFeatureExtraction()
-        model = DecisionTree(extractor)
-
         self.url_input = urlinput
-        print(urlinput)
+        #print(self.url_input)
+        extractor = ApplicationFeatureExtraction()
+        features = extractor.getFeatures(self.url_input)
+        #print(features)
+        model = DecisionTree()
+        decision = model.runUserInput(features)
+        #print(decision)
+        return decision
 
-main = Main()
+        #self.url_input = urlinput
+
+
+
+
+main = Main(extractor=FeatureExtraction())
